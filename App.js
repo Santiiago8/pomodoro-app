b581ee1223209e80b09a4e5c45c87dd5611bf7c3
Santiago Ceballos Palacios <santiagopalcios498@gmail.com>
2023-07-07 15:58:56 -0300

    app terminada

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Platform, SafeAreaView, TouchableOpacity} from 'react-native';
+import React, { useState } from 'react';
+import Header from './src/components/Header';
+import Timer from './src/components/Timer';
+import { useEffect } from 'react';
+
+const colors = ["#F7DC6F","#A2D9CE", "#D7BDE2"];
 
 export default function App() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREAK" );
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if(isActive){
+      interval = setInterval(() => {
+        setTime(time - 1);
+      },1000)
+    } else {
+      clearInterval(interval);
+    }
+
+    if (time === 0) {
+      setIsActive(false);
+      setIsWorking((prev) => !prev);
+      setTime(isWorking ? 300 : 1500);
+    }
+
+    return () => clearInterval(interval);
+  },[isActive, time])
+
+  function handleStartStop(){
+    setIsActive(!isActive);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container, {backgroundColor: colors[currentTime]}]}>
+      <View style={{
+          flex: 1,
+          paddingHorizontal: 15, 
+          paddingTop: Platform.OS === "android" && 30,
+        }}>
+        <Text style={styles.text}>Pomodoro</Text>
+        <Header 
+          currentTime={currentTime} 
+          setCurrentTime={setCurrentTime}
+          setTime={setTime}
+        />
+        <Timer
+          time={time}
+        />
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={handleStartStop}
+        >
+          <Text style={{color: "#FFF", fontWeight: 'bold'}}>
+            
+            {isActive ? "STOP" : "START"}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  text: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: "#333333",
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 15,
+    alignItems: 'center',
+  }
 });
---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+const options = ['Pomodoro', 'Short Break', 'Long Break'];
+
+function Header({setTime, currentTime, setCurrentTime }) {
+
+    function handlePress(index){
+        const newTime = index === 0 ? 25 : index === 1 ? 5 : 15;
+        setCurrentTime(index);
+        setTime(newTime * 60);
+    }
+
+    return (
+        <View style={styles.container}>
+            {options.map((item, index) => ( 
+                <TouchableOpacity 
+                key={index} 
+                onPress={() => {handlePress(index)}}
+                style={[styles.itemStyle, currentTime !== index && {borderColor: 'transparent'}
+                
+                ]} 
+                >
+                <Text style={{fontWeight: 'bold'}}>{item}</Text>
+                </TouchableOpacity>
+            ))}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row', 
+    },
+    itemStyle: {
+        width: "33%",
+        borderWidth: 3,
+        alignItems: 'center',
+        padding: 5,
+        borderRadius: 10,
+        borderColor: '#FFF',
+        marginVertical: 20,
+    }
+})
+
+export default Header;
